cb49e7bb0cf3bd9469dc8f64a505b2f47b41fa6c
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2014-05-14 20:37:51 +0200

    Obsolete example

--- src/test/java/com/nurkiewicz/reactive/S09_Promises.java
+++ src/test/java/com/nurkiewicz/reactive/S09_Promises.java
@@ -2,20 +2,10 @@ package com.nurkiewicz.reactive;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.nurkiewicz.reactive.util.AbstractFuturesTest;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardWatchEventKinds;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
 import java.time.Duration;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -51,45 +41,6 @@ public class S09_Promises extends AbstractFuturesTest {
 			pool.schedule(() -> promise.complete(result), duration.toMillis(), TimeUnit.MILLISECONDS);
 		});
 		return promise;
-	}
-
-	@Test
-	public void promises() throws Exception {
-		final CompletableFuture<Path> future = newFilePromise();
-
-		log.debug("New file found on desktop {}", future.get());
-	}
-
-	private CompletableFuture<Path> newFilePromise() {
-		final CompletableFuture<Path> promise = new CompletableFuture<>();
-
-		new Thread("FileSystemWatcher") {
-			@Override
-			public void run() {
-				try {
-					promise.complete(waitForNewFile());
-				} catch (Exception e) {
-					promise.completeExceptionally(e);
-				}
-			}
-		}.start();
-
-		return promise;
-	}
-
-	private Path waitForNewFile() throws IOException, InterruptedException {
-		WatchService watchService = FileSystems.getDefault().newWatchService();
-		final WatchKey key = Paths.
-				get(System.getProperty("user.home"), "Desktop").
-				register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
-		while (!Thread.currentThread().isInterrupted()) {
-			final List<WatchEvent<?>> events = key.pollEvents();
-			if (!events.isEmpty()) {
-				return (Path) events.get(0).context();
-			}
-			key.reset();
-		}
-		throw new InterruptedException();
 	}
 
 }
