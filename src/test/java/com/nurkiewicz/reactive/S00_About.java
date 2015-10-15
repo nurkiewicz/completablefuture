146acfbf0f23c8f0ca5f2ad62b59f14aff84385e
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2015-10-15 22:06:52 +0200

    About "slide"

---
+++ src/test/java/com/nurkiewicz/reactive/S00_About.java
@@ -0,0 +1,15 @@
+package com.nurkiewicz.reactive;
+
+/**
+ *
+ * Tomasz Nurkiewicz
+ *
+ * @tnurkiewicz | @4financeit
+ *
+ * www.nurkiewicz.com
+ *
+ * www.github.com/nurkiewicz/hystrix-demo
+ *
+ */
+public class S00_About {
+}
